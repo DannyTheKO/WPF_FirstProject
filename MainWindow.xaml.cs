ce15bde041e7d782e83cacd74177755b6eeb0aa4
Danny <thanhan1100@gmail.com>
2024-12-18 16:53:44 +0700

    Add MenuBar user control and update MainWindow layout
    - Updated MainWindow.xaml to include a new namespace `userControls` for custom user controls.
    - Added a new Grid layout with three rows: toolbar, main menu, and footer.
    - Integrated MenuBar user control in the toolbar.
    - Modified MainWindow.xaml.cs to remove unnecessary using directives and comments.
    - Added a `Running` boolean field and commented-out event handlers for button clicks.
    - Created MenuBar.xaml to define a UserControl named `MenuBar` with a Menu, Rectangle, TextBox, and Button.
    - Added MenuBar.xaml.cs to provide code-behind for the MenuBar user control, including necessary using directives and a constructor.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,39 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPF_FirstProject
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainWindow : Window
+	{
+		private bool Running = false;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			//TbHelloWorld.Text = "Hello World, This Has Been Change By An Constructor!";
+		}
+
+		//private void BtnRun_OnClick(object sender, RoutedEventArgs e)
+		//{
+		//	if (!Running)
+		//	{
+		//		// Run
+		//		TbHelloWorld.Text = "Hello Danny!, Running!";
+		//		BtnRun.Content = "Stop";
+		//	}
+		//	else
+		//	{
+		//		// Stop
+		//		TbHelloWorld.Text = "Hello Danny!, Stopped!";
+		//		BtnRun.Content = "Run";
+		//	}
+
+		//	Running = !Running;
+		//}
+
+		//private void BtnHi_OnClick(object sender, RoutedEventArgs e)
+		//{
+		//	TbHelloWorld.Text = "Hi Danny!";
+		//}
+	}
 }
